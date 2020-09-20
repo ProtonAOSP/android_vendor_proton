d422711cdd0b8850c78ec78627ece24a96ede5af
Danny Lin <danny@kdrag0n.dev>
2020-09-20 16:59:54 -0700

    Add custom fonts for enhanced typography
    This adds the Fira Code, Inter, and Source Serif Pro fonts to replace
    Cutive Mono/Droid Sans Mono, Roboto, and Noto Serif respectively. All of
    their respective copyright notices and licenses have been added.
    
    These fonts have been patched to use the same names as the fonts they
    are replacing for maximum compatibility, as some apps (e.g. Firefox
    Fenix) look for fonts by their TTF/OTF name rather than using Android's
    fonts.xml definitions.
    
    All of these fonts are licensed under the Open Font License (OFL).

---
+++ fonts/fonts.mk
@@ -0,0 +1,23 @@
+# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
+# get installed too.
+
+PRODUCT_PACKAGES := \
+    CutiveMono.ttf \
+    DroidSansMono.ttf \
+    Roboto-BlackItalic.ttf \
+    Roboto-Black.ttf \
+    Roboto-BoldItalic.ttf \
+    Roboto-Bold.ttf \
+    Roboto-Italic.ttf \
+    Roboto-LightItalic.ttf \
+    Roboto-Light.ttf \
+    Roboto-MediumItalic.ttf \
+    Roboto-Medium.ttf \
+    Roboto-Regular.ttf \
+    Roboto-ThinItalic.ttf \
+    Roboto-Thin.ttf \
+    NotoSerif-BoldItalic.ttf \
+    NotoSerif-Bold.ttf \
+    NotoSerif-Italic.ttf \
+    NotoSerif-Regular.ttf \
---
+++ fonts/generate_build.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+q='"'
+
+echo '# Copyright (C) 2020 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+' > fonts.mk
+echo '# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
+# get installed too.
+
+PRODUCT_PACKAGES := \' > fonts.mk
+
+rm -f */Android.bp
+
+for src in */*.otf
+do
+    echo "$src"
+
+    font_dir="$(dirname "$src")"
+    src_base="$(basename "$src" .otf)"
+    src_name="$(basename "$src")"
+    if [[ "$src" == *"_"* ]]; then
+    	# If a name was explicitly specified with an underscore, use it
+	android_name="$(cut -d'_' -f2 <<< "$src_base").ttf"
+    else
+    	# Use heuristics to construct a name
+	android_name="$(sed -e 's/Inter/Roboto/; s/SFUIText/Roboto/; s/SourceSerifPro/NotoSerif/' <<< "$src_base").ttf"
+    fi
+
+    if [[ ! -f "$font_dir/Android.bp" ]]; then
+        echo '// Copyright (C) 2020 The Proton AOSP Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+' > "$font_dir/Android.bp"
+    fi
+
+    echo "
+prebuilt_font {
+    name: ${q}$android_name${q},
+    src: ${q}$src_name${q},
+}" >> "$font_dir/Android.bp"
+    echo "    $android_name \\" >> fonts.mk
+
+done
