6859dbef164a97804f8350f23073c3ad1cd00cd4
Danny Lin <danny@kdrag0n.dev>
2020-10-06 17:56:26 -0700

    build: Add support for overriding product info
    Google Play Store will serve debug versions of their apps if it detects
    an aosp_<devicename> device name because they use AOSP device trees
    internally for testing and debugging.
    
    We also need to override the fingerprint and other information to match
    stock in order for Google SafetyNet to pass.

--- build/early.mk
+++ build/early.mk
@@ -15,3 +15,24 @@
 # Custom ROM version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.version.custom=11.0.0
+
+# Override product info for Google Play Services and SafetyNet
+ifeq ($(PRODUCT_OVERRIDE_INFO),true)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.product.system.name=$(PRODUCT_OVERRIDE_NAME) \
+    ro.product.product.name=$(PRODUCT_OVERRIDE_NAME) \
+    ro.product.system_ext.name=$(PRODUCT_OVERRIDE_NAME) \
+    ro.product.odm.name=$(PRODUCT_OVERRIDE_NAME) \
+    ro.product.vendor.name=$(PRODUCT_OVERRIDE_NAME) \
+    ro.build.flavor=$(PRODUCT_OVERRIDE_NAME)-user \
+    ro.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.system.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.product.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.system_ext.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.bootimage.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.odm.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.vendor.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT)
+
+# Description needs special treatment because it contains spaces
+PRIVATE_BUILD_DESC := $(PRODUCT_OVERRIDE_DESC)
+endif
