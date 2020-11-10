ffa359b5921a20d85082552aea13e7df3cdaae95
Danny Lin <danny@kdrag0n.dev>
2020-11-09 16:30:05 -0800

    version: Split custom ROM version into variable
    For future use.

--- version.mk
+++ version.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Custom ROM version
+CUSTOM_ROM_VERSION := 11.2.0
+
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.build.version.custom=11.2.0
+    ro.build.version.custom=$(CUSTOM_ROM_VERSION)
