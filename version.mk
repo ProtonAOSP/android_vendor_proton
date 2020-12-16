14b3c71807724dcde4c8598ee4d67c60d8436bdc
Danny Lin <danny@kdrag0n.dev>
2020-12-15 19:58:58 -0800

    Bump version to 11.3.0
    I decided to bump the minor version after all.

--- version.mk
+++ version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CUSTOM_ROM_VERSION := 11.2.1
+CUSTOM_ROM_VERSION := 11.3.0
 
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.version.custom=$(CUSTOM_ROM_VERSION)
