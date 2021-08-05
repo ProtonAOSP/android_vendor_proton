7230dbe1babbbca7f668d81625e474eeddb504cd
Danny Lin <danny@kdrag0n.dev>
2021-08-05 01:51:10 -0700

    product: Enable Gboard side padding
    This adds padding to the left and right sides of the keyboard when using
    Gboard.
    
    In portrait mode, there is 4dp of padding to align the keyboard with
    some UI elements and separate the keys from the edge of the screen.
    
    In landscape mode, there is 64dp of padding to reduce the overall key
    width, which makes the keyboard significantly easier to use.
    
    TODO: It might be better to set these in individual device trees, but
    for now, these props seem to work well for all official devices.
    
    Suggested-by: Diab Neiroukh <lazerl0rd@thezest.dev>

--- build/product.mk
+++ build/product.mk
@@ -85,3 +85,10 @@ PRODUCT_PACKAGES += \
 ifeq ($(WITH_GMS),true)
 $(call inherit-product, vendor/gapps/common/common-vendor.mk)
 endif
+
+# Gboard side padding
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.kb_pad_port_l=4 \
+    ro.com.google.ime.kb_pad_port_r=4 \
+    ro.com.google.ime.kb_pad_land_l=64 \
+    ro.com.google.ime.kb_pad_land_r=64 \
