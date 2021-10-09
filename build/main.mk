aff91954c8afb9a1fcc517b282e8d7be2133856a
Danny Lin <danny@kdrag0n.dev>
2021-10-09 03:27:11 -0700

    main: Remove build desc override
    SafetyNet doesn't check this.

--- build/main.mk
+++ build/main.mk
@@ -23,8 +23,5 @@ ifeq ($(PRODUCT_OVERRIDE_INFO),true)
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.tags=release-keys \
     ro.build.flavor=$(PRODUCT_NAME)-user \
-    ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
-
-# Description needs special treatment because it contains spaces
-PRIVATE_BUILD_DESC := $(PRODUCT_OVERRIDE_DESC)
+    ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT)
 endif
