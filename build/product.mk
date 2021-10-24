316f86715b63923534d39c4877cc4612dbbe3c9a
Danny Lin <danny@kdrag0n.dev>
2021-10-24 16:36:01 -0700

    product: Enable gestural navigation overlay to match default nav mode
    This fixes an occasional issue where the navbar is in a broken state
    (gesture nav works, but the bar is rendered as if it's 2/3-button nav)
    after wiping data or privisioning new users.

--- build/product.mk
+++ build/product.mk
@@ -41,6 +41,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
 
+# Enable gestural navigation overlay to match default navigation mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+
 # AOSP userdebug/eng CLI tools
 PRODUCT_PACKAGES += \
     arping \
