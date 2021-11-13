bc8501f154d2bb873a05eb81865aae04d2b9cfb3
Danny Lin <danny@kdrag0n.dev>
2021-11-13 14:36:56 -0800

    product: Fix Gboard prop conflicts with Pixel 6
    The Pixel 6 device tree has device-specific padding, especially to
    match the curved screen on the Pixel 6 Pro.

--- build/product.mk
+++ build/product.mk
@@ -103,7 +103,7 @@ endif
 
 # Gboard side padding
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.ime.kb_pad_port_l=4 \
-    ro.com.google.ime.kb_pad_port_r=4 \
-    ro.com.google.ime.kb_pad_land_l=64 \
-    ro.com.google.ime.kb_pad_land_r=64 \
+    ro.com.google.ime.kb_pad_port_l?=4 \
+    ro.com.google.ime.kb_pad_port_r?=4 \
+    ro.com.google.ime.kb_pad_land_l?=64 \
+    ro.com.google.ime.kb_pad_land_r?=64 \
